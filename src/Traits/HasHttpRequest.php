7d1c1707048a465eaa17b3f9f94ab4f6fb529313
yansongda <me@yansongda.cn>
2018-03-22 20:33:46 +0800

    fix some bugs

--- src/Traits/HasHttpRequest.php
+++ src/Traits/HasHttpRequest.php
@@ -110,7 +110,7 @@ trait HasHttpRequest
         if (false !== stripos($contentType, 'json') || stripos($contentType, 'javascript')) {
             return json_decode($contents, true);
         } elseif (false !== stripos($contentType, 'xml')) {
-            return json_decode(json_encode(simplexml_load_string($contents)), true);
+            return json_decode(json_encode(simplexml_load_string($contents, 'SimpleXMLElement', LIBXML_NOCDATA), JSON_UNESCAPED_UNICODE), true);
         }
 
         return $contents;
