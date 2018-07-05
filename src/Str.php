62cc9848ea700fd1cb53bc61e01948d9df7be7f4
yansongda <me@yansongda.cn>
2018-07-05 16:57:20 +0800

    compate php5

--- src/Str.php
+++ src/Str.php
@@ -276,7 +276,7 @@ class Str
         while (($len = strlen($string)) < $length) {
             $size = $length - $len;
 
-            $bytes = random_bytes($size);
+            $bytes = function_exists('random_bytes') ? random_bytes($size) : mt_rand($size);
 
             $string .= substr(str_replace(['/', '+', '='], '', base64_encode($bytes)), 0, $size);
         }
