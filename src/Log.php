27d2692005af9900bc6aa4390bc9bd4a3d6a1fdd
yansongda <me@yansongda.cn>
2019-05-06 17:29:58 +0800

    update

--- src/Log.php
+++ src/Log.php
@@ -2,6 +2,17 @@
 
 namespace Yansongda\Supports;
 
+/**
+ * @method static void emergency($message, array $context = array())
+ * @method static void alert($message, array $context = array())
+ * @method static void critical($message, array $context = array())
+ * @method static void error($message, array $context = array())
+ * @method static void warning($message, array $context = array())
+ * @method static void notice($message, array $context = array())
+ * @method static void info($message, array $context = array())
+ * @method static void debug($message, array $context = array())
+ * @method static void log($message, array $context = array())
+ */
 class Log extends Logger
 {
 }
