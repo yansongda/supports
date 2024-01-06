531e1aa30f96a18a7621df5b84db1684e26abdbb
yansongda <yansongda@users.noreply.github.com>
2024-01-06 12:32:43 +0800

    feat: 增加 wrapQuery 方法 (#22)

--- src/Collection.php
+++ src/Collection.php
@@ -45,6 +45,11 @@ class Collection implements ArrayAccess, Countable, IteratorAggregate, JsonSeria
         return new static(Arr::wrapXml($xml));
     }
 
+    public static function wrapQuery(string $query): self
+    {
+        return new static(Arr::wrapQuery($query));
+    }
+
     public static function unwrap(array|Collection $value): array
     {
         return $value instanceof self ? $value->all() : $value;
